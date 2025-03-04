9ca8c593648b001252a635a4dfc41f0adf3b5ec2
IrinaMuravyova <miomir@yandex.ru>
2025-03-04 15:36:06 +0800

    Initial Commit

---
+++ Patres/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Patres
+//
+//  Created by Irina Muravyeva on 04.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
