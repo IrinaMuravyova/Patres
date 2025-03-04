2aef11ec22f7e59d2cde2505f60b8af203aca466
IrinaMuravyova <miomir@yandex.ru>
2025-03-04 16:09:03 +0800

    Added tableView on View

--- Patres/Main/ViewController.swift
+++ Patres/Main/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        setupUI()
     }
-
-
 }
 
+// MARK: - UISettings
+extension ViewController {
+    func setupUI() {
+        setupTableView()
+    }
+}
+
+// MARK: - TableViewDelegate, TableViewDataSource
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func setupTableView() {
+        view.addSubview(tableView)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            
+        ])
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "TITLE"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+}
