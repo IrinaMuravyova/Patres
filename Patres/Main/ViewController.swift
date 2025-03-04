e8938dfefad7b9a0b76d046ed9b20e91e7bd3a05
IrinaMuravyova <miomir@yandex.ru>
2025-03-04 18:07:21 +0800

    Prepared UI of cell

--- Patres/Main/ViewController.swift
+++ Patres/Main/ViewController.swift
@@ -45,17 +45,90 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         ])
     }
     
+    func setup(_ cell: UITableViewCell) {
+        let userPicture: UIImageView = {
+            let imageView = UIImageView()
+            imageView.image = UIImage(systemName: "person")
+            imageView.contentMode = .scaleAspectFit
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            return imageView
+        }()
+        
+        NSLayoutConstraint.activate([
+            userPicture.widthAnchor.constraint(equalToConstant: 30),
+            userPicture.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        let title: UILabel = {
+            let label = UILabel()
+            label.text = "TITLE"
+            label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+            label.contentMode = .scaleAspectFit
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
+        let text: UILabel = {
+            let label = UILabel()
+            label.contentMode = .scaleAspectFit
+            label.text = "post text"
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
+        let liked: UIImageView = {
+            let imageView = UIImageView()
+            imageView.image = UIImage(systemName: "heart")
+            imageView.contentMode = .scaleAspectFit
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.isUserInteractionEnabled = true
+            return imageView
+        }()
+        
+        let subStackView: UIStackView = {
+            let stack = UIStackView()
+            stack.axis = .horizontal
+            stack.spacing = 10
+            stack.alignment = .center
+            stack.distribution = .fill
+            stack.translatesAutoresizingMaskIntoConstraints = false
+            return stack
+        }()
+        
+        subStackView.addArrangedSubview(userPicture)
+        subStackView.addArrangedSubview(title)
+        
+        let stackView: UIStackView = {
+            let stack = UIStackView()
+            stack.axis = .vertical
+            stack.spacing = 10
+            stack.alignment = .fill
+            stack.distribution = .fill
+            stack.translatesAutoresizingMaskIntoConstraints = false
+            return stack
+        }()
+        
+        stackView.addArrangedSubview(subStackView)
+        stackView.addArrangedSubview(text)
+        stackView.addArrangedSubview(liked)
+        
+        cell.contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant:  -10),
+            stackView.topAnchor.constraint(equalTo: cell.contentView.topAnchor, constant: 10),
+            stackView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: -10)
+        ])
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        var content = cell.defaultContentConfiguration()
-        content.text = "TITLE"
-        cell.contentConfiguration = content
+        setup(cell)
         return cell
     }
-    
-    
 }
