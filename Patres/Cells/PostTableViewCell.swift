11c1d35c2312a72f0ead7b1298c63782e6545914
IrinaMuravyova <miomir@yandex.ru>
2025-03-05 22:52:18 +0800

    I've moved the cell setting to a separate class. Avatars for posts are uploaded from Lorem Picsum, depending on the user's ID.

---
+++ Patres/Cells/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  Patres
+//
+//  Created by Irina Muravyeva on 05.03.2025.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
